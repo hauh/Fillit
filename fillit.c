e6d30484c9e10aafee48f5fbd1fdba7cb9875f44
smorty <hauh@ya.ru>
2019-04-21 19:42:14 +0300

    it's working somehow

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: smorty <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 17:25:30 by smorty            #+#    #+#             */
-/*   Updated: 2019/04/21 19:26:30 by smorty           ###   ########.fr       */
+/*   Updated: 2019/04/21 19:40:26 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,6 +74,25 @@ static void add_piece(char **square, t_tetris **list, int y, int x)
 	}
 }
 
+static void remove_piece(char **square, t_tetris **list, int y, int x)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < (*list)->rows)
+	{
+		j = 0;
+		while (j < (*list)->cols)
+		{
+			if ((*list)->figure[i][j] != '.')
+				square[y + i][x + j] = '.';
+			j++;
+		}
+		i++;
+	}
+}
+
 static int solve(char **square, t_tetris **list, int size, int y, int x)
 {
 	if (*list)
@@ -88,7 +107,10 @@ static int solve(char **square, t_tetris **list, int size, int y, int x)
 					add_piece(square, list, y, x);
 					print_solution(&*square);
 					printf("add next\n");
-					return (solve(square, &(*list)->next, size, 0, 0));
+					if (!solve(square, &(*list)->next, size, 0, 0))
+						return (0);
+					else
+						remove_piece(square, list, y, x);
 				}
 				if (x + 1 + (*list)->cols <= size)
 					return (solve(square, list, size, y, x + 1));
@@ -120,7 +142,7 @@ static char **create_square(unsigned int size)
 	return (square);
 }
 
-void fillit(t_tetris **list)
+void	fillit(t_tetris **list)
 {
 	unsigned int size;
 	char **square;
@@ -130,7 +152,7 @@ void fillit(t_tetris **list)
 	{
 		square = create_square(size);
 		if (!solve(square, list, size, 0, 0))
-			break ;
+			break;
 		size++;
 	}
 }
