79777cb2494c9598167f6ce9bf15884a85d89ed1
smorty <hauh@ya.ru>
2019-04-22 15:30:00 +0300

    fillit tweaks

--- fillit.c
+++ fillit.c
@@ -6,7 +6,7 @@
 /*   By: smorty <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 17:25:30 by smorty            #+#    #+#             */
-/*   Updated: 2019/04/21 21:36:25 by smorty           ###   ########.fr       */
+/*   Updated: 2019/04/22 15:28:03 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,11 +97,15 @@ static int solve(char **square, t_tetris **list, int size, int y, int x)
 	{
 		if (y + (*list)->rows <= size)
 		{
+			while ((square[y][x] != '.') && (square[y][x] != '\0') && ((*list)->figure[0][0] != '.'))
+				x++;
 			if (x + (*list)->cols <= size)
 			{
 				if (!check_spot(square, list, y, x))
 				{
 					add_piece(square, list, y, x);
+					print_solution(square);
+					ft_putchar('\n');
 					if (!solve(square, &(*list)->next, size, 0, 0))
 						return (0);
 					else
@@ -136,41 +140,40 @@ static char **create_square(unsigned int size)
 	return (square);
 }
 
-void	fillit(t_tetris **list)
+static void free_square(char **square)
+{
+	int i;
+
+	i = 0;
+	while (square[i])
+	{
+		free(square[i]);
+		i++;
+	}
+	free(square);
+}
+
+static int starting_size(int count)
+{
+	int n;
+
+	n = 2;
+	while (n * n < count * 4)
+		n++;
+	return (n);
+}
+
+void	fillit(t_tetris **list, int count)
 {
 	unsigned int size;
 	char **square;
-	unsigned int x;
-	unsigned int y;
-	int res;
 
-	size = 3;
-	while (size < 20)
+	size = starting_size(count);
+	square = create_square(size);
+	while (solve(square, list, size, 0, 0))
 	{
-		y = 0;
-		while (y < size)
-		{
-			x = 0;
-			while (x < size)
-			{
-				square = create_square(size);
-				res = solve(square, list, size, y, x);
-				if (!res)
-				{
-					ft_putstr("square size: ");
-					ft_putnbr(size);
-					ft_putchar('\n');
-					print_solution(&*square);
-					break;
-				}
-				x++;
-			}
-			if (!res)
-				break;
-			y++;
-		}
-		if (!res)
-			break;
+		free_square(square);
 		size++;
+		square = create_square(size);
 	}
 }
--- main.c
+++ main.c
@@ -6,12 +6,12 @@
 /*   By: smorty <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 15:58:37 by smorty            #+#    #+#             */
-/*   Updated: 2019/04/21 21:37:55 by smorty           ###   ########.fr       */
+/*   Updated: 2019/04/22 14:47:28 by smorty           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
-
+/*
 static void print_figures(t_tetris **list)
 {
 	int i;
@@ -28,18 +28,27 @@ static void print_figures(t_tetris **list)
 		print_figures(&(*list)->next);
 	}
 }
-
+*/
 int		main(int argc, char **argv)
 {
 	int fd;
+	int count;
 	t_tetris *new;
+	t_tetris *tmp;
 
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
 		new = store_tetris(fd);
-		print_figures(&new);
-		fillit(&new);
+		count = 0;
+		tmp = new;
+		while (tmp)
+		{
+			count++;
+			tmp = tmp->next;
+		}
+//		print_figures(&new);
+		fillit(&new, count);
 	}
 	return (0);
 }
